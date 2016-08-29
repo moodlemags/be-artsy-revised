3b66145be18aee40cfe1f93fb0083d9ff49ce5bf
Maggie Merrell <maggiemerrell31@gmail.com>
2016-08-29 11:33:28 -0400

    second day of work - game page

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,57 @@
+class GameController < ApplicationController
+  require 'hyperclient'
+
+  def index
+    client_id = '45c1a28f62560c431645'
+    client_secret = '3fb01aaf998e1ef3fea830d232a36c05'
+
+    api = Hyperclient.new('https://api.artsy.net/api') do |api|
+      api.headers['Accept'] = 'application/vnd.artsy-v2+json'
+      api.headers['Content-Type'] = 'application/json'
+      api.connection(default: false) do |conn|
+        conn.use FaradayMiddleware::FollowRedirects
+        conn.use Faraday::Response::RaiseError
+        conn.request :json
+        conn.response :json, content_type: /\bjson$/
+        conn.adapter :net_http
+      end
+    end
+    xapp_token = api.tokens.xapp_token._post(client_id: client_id, client_secret: client_secret).token
+    puts "token:", xapp_token
+    render json: {'Welcome controller': 'welcome to the Artsy API woowooowow'}
+  end
+
+  def get_game_data
+    # gene_received = params[:id]
+    # puts "gene param #{gene_received}"
+    xapp_token = 'JvTPWe4WsQO-xqX6Bts49id7qJqr5JLsQGAdvulqJSW9mftRPIKGBuNJnrA0JfQ8-dFZSrZeKVEBsJGHtE6lIT1otchC4Yw6uavJ3kY7STUddLq2MkVxx7KoIKFD3oTy2ltss5IeJT2lhszx6oM0W9YK1nm3aoC8F1Nxo4LPfiYaAhNW_k4W2GrspCa2SZfpZbmxT351ecFpgcfId8mq3iq5sfRkVMy5V8zs4nskkx4='
+
+      api = Hyperclient.new('https://api.artsy.net/api') do |api|
+        api.headers['Accept'] = 'application/vnd.artsy-v2+json'
+        api.headers['X-Xapp-Token'] = xapp_token
+        api.connection(default: false) do |conn|
+          conn.use FaradayMiddleware::FollowRedirects
+          conn.use Faraday::Response::RaiseError
+          conn.request :json
+          conn.response :json, content_type: /\bjson$/
+          conn.adapter :net_http
+        end
+      end
+
+      painting_array = ["521d22b9139b213ff0000334", "4d8b937c4eb68a1b2c001722", "53d128877261692cddbd0000", "4d8b92eb4eb68a1b2c000968"]
+      main_painting = painting_array.sample
+      puts "'random painting',#{main_painting}"
+      api_link = api.artwork(id: main_painting)
+      puts "link, #{api_link}"
+      data = {
+        painting_id: "#{api_link._links.thumbnail}"
+      }
+
+      render json: data
+      # sample_renaissance = api.gene(id: gene_received)
+      # puts "'api endpoint', #{sample_renaissance}"
+
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ get   "/"       => "artsy#index"
 post  "/artsy"  => "artsy#trigger_artsy_api"
 get   "/artsy"  => "artsy#index"
 
+get "/game" => "game#index"
+post "/game" => "game#get_game_data"
+
 end
